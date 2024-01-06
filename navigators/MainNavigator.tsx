de82f9c131061797962b624b990b7de0d0f428f6
marcruzsec <133927883+marcruzsec@users.noreply.github.com>
2024-01-05 21:24:23 -0500

    Navegadores

--- navigators/MainNavigator.tsx
+++ navigators/MainNavigator.tsx
@@ -1,12 +1,41 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import InicioSesionScreen from '../screens/InicioSesionScreen';
+import RegistroScreen from '../screens/RegistroScreen';
+import PuntajeScreen from '../screens/PuntajeScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function MainNavigator() {
-  return (
-    <View>
-      <Text>MainNavigator</Text>
-    </View>
-  )
+const Tabs= createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function MyTabs(){
+    return(
+    <Tabs.Navigator>
+   <Tabs.Screen name="Inicio Sesión" component={InicioSesionScreen}/>
+   <Tabs.Screen name="Registarse" component={RegistroScreen}/>
+   <Tabs.Screen name="Puntaje" component={PuntajeScreen}/>
+    </Tabs.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({})
+
+
+function MyStack() {
+    return (
+      <Stack.Navigator initialRouteName='Welcome'>
+        <Stack.Screen name="Tabs" component={MyTabs} />
+        <Stack.Screen name='Welcome' component={WelcomeScreen}/>
+      </Stack.Navigator>
+    );
+  }
+
+export default function MainNavigators() {
+  return (
+    <MainNavigators>
+      <MyStack/>
+    </MainNavigators>
+  )
+}
--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function WelcomeScreen() {
-  return (
-    <View>
-      <Text>WelcomeScreen</Text>
-    </View>
-  )
-}
+export default function WelcomeScreen({navigation}: any) {
+    return (
+      <View>
+        <Text>Bienvenidos</Text>
+        <Button title= 'Jugar' onPress={()=> navigation.navigate('Tabs')}/>
+      </View>
+    )
+  }
+  
+  const styles = StyleSheet.create({})
